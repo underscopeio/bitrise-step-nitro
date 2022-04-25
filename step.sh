509073438bb30ec9c4e978ed4d3ecdbbd2422929
Juan Pablo Garcia <juanpablogarcia@gmail.com>
2022-04-25 16:35:07 -0300

    Bump version to 0.0.2-alpha.3

--- step.sh
+++ step.sh
@@ -8,7 +8,7 @@ fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-STEP_VERSION=0.0.2-alpha.2
+STEP_VERSION=0.0.2-alpha.3
 
 MACOS_BIN_FILE="uci-macos"
 LINUX_BIN_FILE="uci-linux"
